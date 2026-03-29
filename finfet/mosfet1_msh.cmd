29269060615ad2cd6c8096aa3e560d50456fc6a8
Varun R <65390763+varunnaikr@users.noreply.github.com>
2026-03-29 16:11:34 +0530

    Create full mesh generation configuration file for MOSFET under finfet directory

---
+++ finfet/mosfet1_msh.cmd
@@ -0,0 +1,24 @@
+# Full Mesh Generation Configuration File for MOSFET
+
+## Title
+Title: Full Mesh Generation for MOSFET
+
+## Controls
+Controls:
+  - Mesh Generation: Enabled
+  - Refinement Level: High
+
+## IOControls
+IOControls:
+  - Input File: input.msh
+  - Output File: output.msh
+
+## Definitions
+Definitions:
+  - Device Type: MOSFET
+  - Geometry: Standard
+
+## Placements
+Placements:
+  - Placement: Origin
+  - Orientation: Standard
